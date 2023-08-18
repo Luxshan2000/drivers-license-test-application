d2ee2ff430e4a705e1022e4e57aef4be14b1e732
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-08-19 03:59:29 +0530

    initial

--- webapp/src/components/NarbarComponent.js
+++ webapp/src/components/NarbarComponent.js
@@ -2,23 +2,8 @@ import React from 'react'
 import './styles/navbarStyle.css'
 
 function NarbarComponent() {
-
-  const navStyle = {
-    position: 'sticky',
-    top: 0,
-    width: '100%',
-    boxSizing: 'border-box',
-    display: 'flex',
-    padding: '0.6em',
-    margin: '0',
-    backgroundColor: 'green',
-    alignFtems: 'center',
-    justifyFontent: 'space-between',
-    fontFamily: 'Arial, sans-serif',
-    userSelect: 'none',
-  }
   return (
-    <div style={navStyle} >NarbarComponent</div>
+    <div  className='navStyle' >Narbar Component</div>
   )
 }
 
