7f3cd5a98776df5d3b10dd9bac6ca30e8b911734
kamadi2000 <kmaliyanage@gmail.com>
2023-08-19 18:07:32 +0530

    add home-page navigation bar

--- webapp/src/components/NarbarComponent.js
+++ webapp/src/components/NarbarComponent.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import './styles/navbarStyle.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function NarbarComponent() {
   return (
-    <div  className='navStyle' >Narbar Component</div>
+    <div  className='navStyle' >
+       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about us">About us</Nav.Link>
+            <Nav.Link href="#contact us">Contact us</Nav.Link>
+            <Nav.Link href="#signup">Sign up</Nav.Link>
+            <Nav.Link href="login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </div>
   )
 }
 
