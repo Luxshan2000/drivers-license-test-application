b9d79aaf6992bc552d103c0bca1ea04a569c1033
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-08-21 05:03:58 +0530

    heading-updated

--- webapp/src/components/HeadingComponent.js
+++ webapp/src/components/HeadingComponent.js
@@ -1,38 +1,30 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../assets/CSS/headingStyle.css'
 
 
 
-function HeadingComponent() {
-  const heading = {
-    name: "2. Who can apply for driving license?",
-    status:"Completed!",
-    availability:"Free",
-    video:"URL Video",
-    quiz:"QuizId",
-    subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
-  }
+function HeadingComponent({heading}) {
+
   return (
-    <div className=' shadow  bg-success container rounded-3  p-3 justify-content-center'>
+    <div className='hover-effect m-3  shadow-sm  p-3  container rounded-3   justify-content-center'>
         <div 
+          style={{cursor:'pointer'}}
           data-bs-toggle="collapse" 
-          data-bs-target="#content" 
+          data-bs-target={`#${heading.id}`} 
           aria-expanded="false" 
-          aria-controls="content">
-          <div className=' row'>
-            <h4>{heading.name}</h4>
-          </div>
-          <div className=' row'>
-            <h5 className=' col-6'>{heading.status}</h5>
-            <h5 className=' col-6'>{heading.availability}</h5>
+          aria-controls={`${heading.id}`}>
+          <div className=' text-start row'>
+            <h5>{heading.name}</h5>
           </div>
         </div>
-        <div class="collapse" id="content">
-          <div className='rounded-3 ' style={{backgroundColor:"lightgreen"}} >
-            <div  className=' p-3 row'>
-              <h5 className=' col-12 col-md-6 col-lg-6 text-start'>{heading.subHeadings[0]}</h5>
-              <h5 className=' col-12 col-md-6 col-lg-6 text-sm-start text-md-end text-lg-end'>{heading.subHeadings[1]}</h5>
+        <div class="collapse" id={`${heading.id}`}>
+          <div className='rounded-3 p-3 pb-0 '  >
+            <div  className=' row'>
+              <hr/>
+              <h5  className='   col-12 col-md-6 col-lg-6 text-start'><a href='/' style={{cursor:'pointer'}}>{heading.subHeadings[0]}</a></h5>
+              <h5  className='    col-12 col-md-6 col-lg-6 text-start text-md-end text-lg-end'><a href='/' style={{cursor:'pointer'}}>{heading.subHeadings[1]}</a></h5>
             </div>
           </div>
         </div>
--- webapp/src/views/CourseView.js
+++ webapp/src/views/CourseView.js
@@ -2,9 +2,91 @@ import React from 'react'
 import HeadingComponent from '../components/HeadingComponent'
 
 function CourseView() {
+  const heading = [
+    {
+      id:"1",
+      name: "1. Overview",
+      status:"Completed!",
+      availability:"Free",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+    },
+    {
+      id:"2",
+      name: "2. Who can apply for driving license?",
+      status:"Completed!",
+      availability:"Free",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+    },
+    {
+      id:"3",
+      name: "3. What are the ways to apply License?",  
+      status:"Completed!",
+      availability:"Free",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+    },
+    {
+      id:"4",
+      name: "4. Sample heading",  
+      status:"Process!",
+      availability:"Premium",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+    },
+    {
+      
+      id:"5",
+      name: "5. Sample heading",  
+      status:"Process!",
+      availability:"Premium",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+      
+    },
+    {
+      
+      id:"6",
+      name: "6. Sample heading",  
+      status:"Process!",
+      availability:"Premium",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+      
+    },
+    {
+      
+      id:"7",
+      name: "7. Sample heading",  
+      status:"Process!",
+      availability:"Premium",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+      
+    },
+    {
+      
+      id:"8",
+      name: "8. Sample heading",  
+      status:"Process!",
+      availability:"Premium",
+      video:"URL Video",
+      quiz:"QuizId",
+      subHeadings:["1.1 Interactive Video", "1.2 Quiz"]
+      
+    }]
+
   return (
-    <div className=' container-fluid bg-secondary-subtle  p-5'>
-      <HeadingComponent />
+    <div className=' container-fluid   p-5'>
+      {heading.map((item) =><HeadingComponent heading={item} /> )}
     </div>
   )
 }
