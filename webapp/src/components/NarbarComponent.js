5752aba2a13ddf15b72685f3be0a6a51ffc9e555
kamadi2000 <kmaliyanage@gmail.com>
2023-08-21 23:26:07 +0530

    Nav bar small chnages

--- webapp/src/components/NarbarComponent.js
+++ webapp/src/components/NarbarComponent.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../assets/CSS/navbarStyle.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function NarbarComponent() {
   return (
     <div  className='navStyle' >
-       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+       <Navbar scrolling dark collapseOnSelect expand="md" className="bg-body-tertiary" fixed='top'>
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -14,7 +17,7 @@ function NarbarComponent() {
             <Nav.Link href="#about us">About us</Nav.Link>
             <Nav.Link href="#contact us">Contact us</Nav.Link>
             <Nav.Link href="#signup">Sign up</Nav.Link>
-            <Nav.Link href="login">Login</Nav.Link>
+            <Nav.Link href="#login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
