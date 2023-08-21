0a2cfc179b48f4a29a5f96ca7deac736014ffe8e
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-08-21 16:49:16 +0530

    video-component-updating

--- webapp/src/components/VideoComponent.js
+++ webapp/src/components/VideoComponent.js
@@ -1,8 +1,62 @@
-import React from 'react'
+import SampleVideo from '../assets/video/sample.mp4'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFlagCheckered, 
+  faPlay , 
+  faPause, 
+  faClockRotateLeft,
+  faExpand, 
+  faVolumeHigh,
+  faGauge
+} from '@fortawesome/free-solid-svg-icons'
+
 
 function VideoComponent() {
   return (
-    <div>VideoComponent</div>
+    <div className=' container  user-select-none'>
+      <div className=' row '>
+        <video  width={500} src={SampleVideo}  autoPlay />
+      </div>
+      <div className='align-items-center pt-0 pb-0 rounded-bottom-3 d-flex flex-row bg-dark text-white'>
+        <div className="p-lg-3 p-2  text-start flex-shrink-1">
+          <FontAwesomeIcon icon={faPlay} />
+        </div>
+        <div style={{display:"none"}} className="p-3  text-start flex-shrink-1">
+          <FontAwesomeIcon icon={faPause} />
+        </div>
+        <div className="p-lg-3 p-2  text-start flex-shrink-1">
+          <FontAwesomeIcon icon={faClockRotateLeft} />
+        </div>
+        <div className="p-lg-3 p-2 flex-grow-1  flex-shrink-1">
+          <div style={{height:'6px'}} className="progress-stacked ">
+            <div className="progress" role="progressbar" ariaLabel="Segment one" ariaValuenow="40" ariaValuemin="0" ariaValuemax="100" style={{width: "40%"}}>
+              <div className="progress-bar bg-info"></div>
+            </div>
+
+            <div className="progress" role="progressbar" ariaLabel="Segment one" ariaValuenow="0.5" ariaValuemin="0" ariaValuemax="100" style={{width: "0.5%"}}>
+              <div className="progress-bar"></div>
+            </div>
+
+            <div className="progress" role="progressbar" ariaLabel="Segment one" ariaValuenow="10" ariaValuemin="0" ariaValuemax="100" style={{width: "10%"}}>
+              <div className="progress-bar bg-info"></div>
+            </div>
+          </div>
+        </div>
+        <div className='p-lg-3 p-2 text-start  flex-shrink-1'>
+            <FontAwesomeIcon icon={faFlagCheckered} />
+        </div>
+        <div className="p-lg-3 p-2  flex-shrink-1">
+          <FontAwesomeIcon icon={faGauge} />
+        </div>
+        <div className="p-lg-3 p-2  flex-shrink-1">
+          <FontAwesomeIcon icon={faVolumeHigh} />
+        </div>
+        <div className="p-lg-3 p-2  flex-shrink-1"> 
+          <FontAwesomeIcon icon={faExpand} />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- webapp/src/views/ContentView.js
+++ webapp/src/views/ContentView.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import SampleVideo from '../assets/video/sample.mp4'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import VideoComponent from '../components/VideoComponent'
 
 function ContentView() {
   return (
-    <div>ContentView</div>
+    <div className='p-5 container-fluid'>
+      <VideoComponent />
+    </div>
   )
 }
 
