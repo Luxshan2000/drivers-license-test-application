8e5b0e51a4476e25f43df2cf96c2d30824337f12
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-08-22 13:50:28 +0530

    reviewed

--- webapp/src/components/NarbarComponent.js
+++ webapp/src/components/NarbarComponent.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import '../assets/CSS/navbarStyle.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import '../assets/CSS/navbarStyle.css'
 
 function NarbarComponent() {
   return (
     <div  className='navStyle' >
-       <Navbar scrolling dark collapseOnSelect expand="md" className="bg-body-tertiary" fixed='top'>
+       <Navbar scrolling dark collapseOnSelect expand="md" className="bg-body-tertiary " >
       <Container>
         <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -15,7 +15,7 @@ function NarbarComponent() {
           <Nav className="ms-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#about us">About us</Nav.Link>
-            <Nav.Link href="#contact us">Contact us</Nav.Link>
+            <Nav.Link href="/content">Contact us</Nav.Link>
             <Nav.Link href="#signup">Sign up</Nav.Link>
             <Nav.Link href="#login">Login</Nav.Link>
           </Nav>
