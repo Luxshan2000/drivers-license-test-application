5e8efb71565eb99bd71a67df766cbdd3e42cc7f3
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-03 00:24:05 +0530

    quiz-updated

--- webapp/src/components/QuestionComponent.js
+++ webapp/src/components/QuestionComponent.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
-function QuestionComponent() {
+function QuestionComponent({id}) {
   const mcq = {
     ques:"1. What does a circular road sign with a red border and a white center indicate?",
     ans:["Yield to oncoming traffic", "No entry","Give way to pedestrians","Speed limit ahead"]
   }
   return (
-    <div>
-      <h6>{mcq.ques}</h6>
-      <ul>
-        {mcq.ans.map((ans) => <li>{ans}</li>)}
+    <div className=' mb-3'>
+      <h6 className=' p-0 m-0'>{mcq.ques}</h6>
+      <ul style={{padding:'0',margin:'0',marginLeft:'5px'}}>
+        {mcq.ans.map((ans,index) => 
+        <li style={{listStyleType:'none'}} >
+          <input className="form-check-input me-2" type="radio" name={id} id={id}/>
+          <label className="form-check-label" htmlFor={id}>
+            {ans}
+          </label>
+        </li>)}
       </ul>
     </div>
   )
--- webapp/src/views/Exam.js
+++ webapp/src/views/Exam.js
@@ -5,14 +5,14 @@ function Exam() {
   return (
     <div className='container min-vh-100 rounded-1 mb-1 mb-md-2 mb-lg-3 mt-1 mt-md-2 mt-lg-3 col-11 col-md-10 col-lg-10 mx-auto' >
         <div className=' row'>
-            <div className=' ps-2 pe-2 p-1 rounded-1  bg-primary text-center'>
-                <h5>Quiz for Road sign</h5>
+            <div className=' ps-2 pe-2 p-1 rounded-1   text-center'>
+                <h5 className='PageHeading'>Quiz for Road sign</h5>
             </div>
         </div>
-        <div className=' row'>
-            <div className=' min-vh-100 mt-2 ps-2 pe-2  rounded-1  bg-primary-subtle p-2'>
+        <div style={{backgroundColor:"rgb(225, 238, 254)"}} className=' row rounded-2 mt-2 p-2'>
+            <div  className=' min-vh-100'>
                 <div className=' container-fluid' >
-                    <div className=' row'>
+                    <div style={{backgroundColor:"#004053bc"}} className=' row  rounded-2 p-3'>
                         <div className='col-12 col-md-9 col-lg-9 text-start'>
                             <h6>Started On: 22 August 2023, 11.09 PM</h6>
                             <h6>Status: Ongoing</h6>
@@ -20,19 +20,19 @@ function Exam() {
                         <div className='col-12 col-md-3 col-lg-3 text-md-end text-lg-end'>
                             <h6>Remaining: 10 min, 23 Sec</h6>
                         </div>
-                        <hr/>
                     </div>
                     <div>
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
-                        <QuestionComponent />
+                        <hr/>
+                        <QuestionComponent id={1} />
+                        <QuestionComponent id={2} />
+                        <QuestionComponent id={3} />
+                        <QuestionComponent id={4} />
+                        <QuestionComponent id={5} />
+                        <QuestionComponent id={6} />
+                        <QuestionComponent id={7} />
+                        <QuestionComponent id={8} />
+                        <QuestionComponent id={9} />
+                        <QuestionComponent id={10} />
                     </div>
                 </div>
             </div>
@@ -42,9 +42,9 @@ function Exam() {
                 <div className=' row'>
                     <div className=' col-6 col-md-8 col-lg-9  text-start'>
                     </div>
-                    <div className='col-6 col-md-4 col-lg-3 rounded-2 p-2  text-center bg-primary'>
-                        <h6 className=' p-0 m-0'>Finish and Submit</h6>
-                    </div>
+                    <button className=' btn-success col-6 btn col-md-4 col-lg-3 rounded-2 p-2  text-center'>
+                        Finish and Submit
+                    </button>
                 </div>
             </div>
         </div>
