fb05d26ba575fa42010ad11a22a124876c01205c
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-04 01:08:03 +0530

    demo

--- webapp/src/views/Demo.js
+++ webapp/src/views/Demo.js
@@ -4,18 +4,18 @@ function Demo() {
   return (
     <div className='container pt-4  min-vh-100  bg-danger'>
         <div className=' row mb-2'>
-            <div className=' mx-auto mb-2 col-12 col-md-5  p-5 bg-primary'>
+            <div className=' rounded-2 mx-auto mb-2 col-12 col-md-5  p-5 bg-primary'>
                 sample Card
             </div>
-            <div className='mx-auto mb-2 col-12  col-md-5 p-5 bg-primary'>
+            <div className='rounded-2 mx-auto mb-2 col-12  col-md-5 p-5 bg-primary'>
                 sample Card
             </div>
         </div>
         <div className=' row'>
-        <   div className=' mx-auto mb-2  col-12  col-md-5 p-5 bg-primary'>
+        <   div className='rounded-2 mx-auto mb-2  col-12  col-md-5 p-5 bg-primary'>
                 sample Card
             </div>
-            <div className='mx-auto mb-2 col-12 col-md-5 p-5 bg-primary'>
+            <div className='rounded-2 mx-auto mb-2 col-12 col-md-5 p-5 bg-primary'>
                 sample Card
             </div>
         </div>
