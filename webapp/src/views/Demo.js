3c361b184319160c8323fa467da0459b30a05338
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-04 01:13:53 +0530

    demo

--- webapp/src/views/Demo.js
+++ webapp/src/views/Demo.js
@@ -2,21 +2,23 @@ import React from 'react'
 
 function Demo() {
   return (
-    <div className='container pt-4  min-vh-100  bg-danger'>
-        <div className=' row mb-2'>
-            <div className=' rounded-2 mx-auto mb-2 col-12 col-md-5  p-5 bg-primary'>
-                sample Card
+    <div className='container min-vh-100 d-flex align-items-center justify-content-center' >
+        <div className='col-12'>
+            <div className=' row mb-2'>
+                <div className=' rounded-2 mx-auto mb-2 col-12 col-md-5  p-5 bg-primary'>
+                    sample Card
+                </div>
+                <div className='rounded-2 mx-auto mb-2 col-12  col-md-5 p-5 bg-primary'>
+                    sample Card
+                </div>
             </div>
-            <div className='rounded-2 mx-auto mb-2 col-12  col-md-5 p-5 bg-primary'>
-                sample Card
-            </div>
-        </div>
-        <div className=' row'>
-        <   div className='rounded-2 mx-auto mb-2  col-12  col-md-5 p-5 bg-primary'>
-                sample Card
-            </div>
-            <div className='rounded-2 mx-auto mb-2 col-12 col-md-5 p-5 bg-primary'>
-                sample Card
+            <div className=' row'>
+            <   div className='rounded-2 mx-auto mb-2  col-12  col-md-5 p-5 bg-primary'>
+                    sample Card
+                </div>
+                <div className='rounded-2 mx-auto mb-2 col-12 col-md-5 p-5 bg-primary'>
+                    sample Card
+                </div>
             </div>
         </div>
     </div>
