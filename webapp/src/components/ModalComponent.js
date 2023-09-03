17b05b31cbc1675a59d7dd1bcec23a669075fe1b
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-09-04 03:24:12 +0530

    link-updated

--- webapp/src/components/ModalComponent.js
+++ webapp/src/components/ModalComponent.js
@@ -1,39 +1,52 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { Navigate } from 'react-router-dom';
+import "../assets/CSS/modelStyle.css"
 
 function ModalComponent() {
   const [show, setShow] = useState(true);
+  const [move,setMove] = useState(false)
+  const offClick = false
   const isCentered = true;
+  const heading = "Start Attempt"
   const cancelButtonName = "Close"
   const okButtonName = "Start"
   const isClosable =true
   const children = "Quiz will be automatically submitted once the allocated time is over."
-
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+
+  const handleSuccess =()=>{
+    setMove(true)
+  }
+
   return (
     <>
+      {move && <Navigate to='/quiz/exam' />}
       <Modal
         show={show}
         onHide={handleClose}
-        backdrop="static"
+        backdrop={offClick ? true :'static' }
         keyboard={false}
         centered ={isCentered}
+        contentClassName='custom-model'
+      
       >
         <Modal.Header closeButton={isClosable}>
-          <Modal.Title>Start Attempt</Modal.Title>
+          <Modal.Title className='text-center w-100'>{heading}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+
+        {children && <Modal.Body style={{textAlign:'center'}}>
           {children}
-        </Modal.Body>
+        </Modal.Body>}
+
         <Modal.Footer>
           {isClosable && <Button className=' col-4 mx-auto' variant="secondary" onClick={handleClose}>
             {cancelButtonName}
           </Button>}
-              <Button className=' col-4 mx-auto' variant="primary">{okButtonName}
+          <Button onClick={handleSuccess} className=' col-4 mx-auto' variant="success">{okButtonName}
           </Button>
         </Modal.Footer>
       </Modal>
--- webapp/src/components/NarbarComponent.js
+++ webapp/src/components/NarbarComponent.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../assets/CSS/navbarStyle.css'
-
+import { Link } from 'react-router-dom';
 function NarbarComponent() {
   return (
     <div  className='navStyle' >
@@ -13,11 +13,11 @@ function NarbarComponent() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#about us">About us</Nav.Link>
-            <Nav.Link href="/#contact us">Contact us</Nav.Link>
-            <Nav.Link href="/signUp">Sign up</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Item style={{padding:"0 1em 0 1em"}} ><Link to='/'>Home</Link></Nav.Item>
+            <Nav.Item style={{padding:"0 1em 0 1em"}}><Link to='#aboutus'>About us</Link></Nav.Item>
+            <Nav.Item style={{padding:"0 1em 0 1em"}}><Link to='#contactus'>Contact us</Link></Nav.Item>
+            <Nav.Item style={{padding:"0 1em 0 1em"}}><Link to='/signup'>Sign up</Link></Nav.Item>
+            <Nav.Item style={{padding:"0 1em 0 1em"}}><Link to='/login'>Login</Link></Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
