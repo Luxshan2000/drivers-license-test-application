2bdbc201a080595241070ece4cc4de1d0712ed4c
kamadi2000 <kmaliyanage@gmail.com>
2023-09-03 20:42:04 +0530

    Changes in Homepage-carousal

--- webapp/src/components/CarousalCard.js
+++ webapp/src/components/CarousalCard.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Card from 'react-bootstrap/Card';
 
-
-export default function CarousalCard({ icon, description }) {
+export default function CarousalCard({ icon, description, title }) {
     return (
-        <div class="card" style={{ width: '40%',margin : 50 ,boxShadow : 3}}>
-            <div>
-            <i class={icon} style={{ fontSize: 50 ,color:"#3B474F" }}></i>
-            </div>
-            <div class="card-body">
-                <p class="card-text">
-                    {description}    
-                </p>
-            </div>
-        </div>
+        
+            <Card style={{margin:50,height:'35vh'}}>
+                <Card.Body>
+                    <Card.Title style={{textAlign:'center'}}>{title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                        <div style={{ padding: '20px',display:'flex',justifyContent:'center',alignItems:'center' }}>
+                            <i class={icon} style={{ fontSize: 50, color: "#3B474F" }}></i>
+                        </div>
+                    </Card.Subtitle>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+           
     )
 }
