ceb0febb6cc713457179283d6e58a5793e1a1767
OshnMdw <gchoshanmadawa@gmail.com>
2023-09-04 02:01:21 +0530

    Responsive

--- webapp/src/views/PremiumView.js
+++ webapp/src/views/PremiumView.js
@@ -19,13 +19,13 @@ const PremiumView = () => {
         </div>
         <div className='premium__content'>
           <div className='premium__paragraph'>
-            <h2>UPGRADE TO PREMIUM</h2>
-            <h4>Boost your learning today - go premium with a one-time payment of 400.00 LKR. 
-              Get access to exclusive study materials, videos, and interactive quizzes!</h4>
-            <h4>අදම ඔබේ ඉගෙනීම ඉහළ නංවන්න - වාරිකය 400/= කට පමණයි. සුවිශේෂී අධ්‍යයන ද්‍රව්‍ය, 
-              වීඩියෝ සහ අන්තර්ක්‍රියාකාරී ප්‍රශ්නාවලිය වෙත ප්‍රවේශය ලබා ගන්න!</h4>
-            <h4>இன்றே உங்கள் கற்றலை அதிகரிக்கவும் 400.00 LKR ஒரு முறை செலுத்துவதன் மூலம் பிரீமியத்திற்கு செல்லுங்கள். 
-              பிரத்தியேக வீடியோக்கள் மற்றும் வினாடி வினாக்களுக்கான அணுகலைப் பெறுங்கள்!</h4>
+            <h4>UPGRADE TO PREMIUM</h4>
+            <h6>Boost your learning today - go premium with a one-time payment of 400.00 LKR. 
+              Get access to exclusive study materials, videos, and interactive quizzes!</h6>
+            <h6>අදම ඔබේ ඉගෙනීම ඉහළ නංවන්න - වාරිකය 400/= කට පමණයි. සුවිශේෂී අධ්‍යයන ද්‍රව්‍ය, 
+              වීඩියෝ සහ අන්තර්ක්‍රියාකාරී ප්‍රශ්නාවලිය වෙත ප්‍රවේශය ලබා ගන්න!</h6>
+            <h6>இன்றே உங்கள் கற்றலை அதிகரிக்கவும் 400.00 LKR ஒரு முறை செலுத்துவதன் மூலம் பிரீமியத்திற்கு செல்லுங்கள். 
+              பிரத்தியேக வீடியோக்கள் மற்றும் வினாடி வினாக்களுக்கான அணுகலைப் பெறுங்கள்!</h6>
           </div>
           <div>
             <input type="checkbox" id="agree" onChange={checkboxHandler} />
