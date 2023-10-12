e53a629a4a7243c6b7b736c68e01c1587d3afc58
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-10-12 10:31:43 +0530

    database schema added

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Topic = require('../src/models/topic');
 
 
 const connectMongoDb = ()=>{
@@ -10,6 +11,42 @@ const connectMongoDb = ()=>{
         console.log('====================================');
         console.log("mongoDB is sucessfully connected!");
         console.log('====================================');
+
+                    /*Debugging Purpose*/
+        // const sample  = async ()=>{
+        // const title = "Overview"
+        // const no = 1
+        // const videoUrl = "videourl"
+        // const script = '<h1 >This is an script</h1>'
+        // const questions = [
+        //     {
+        //       ques: "Question 1",
+        //       answer: "Answer 1",
+        //       posFeedback: "Correct!",
+        //       negativeFeedback: "Sorry, wrong answer.",
+        //       options: ["Option A", "Option B", "Option C"],
+        //       correctOption: "Option B"
+        //     },
+        //     {
+        //       ques: "Question 2",
+        //       answer: "Answer 2",
+        //       posFeedback: "Good job!",
+        //       negativeFeedback: "Try again.",
+        //       options: ["Option X", "Option Y", "Option Z"],
+        //       correctOption:"Option X"
+        //     }
+        //     // Add more questions as needed
+        //   ]
+
+
+        // const topic = new Topic({title,no,videoUrl,  script, questions })
+
+        //  await topic.save()
+        // }
+
+        // sample()
+
+
     }).catch((error)=>{
         console.log('====================================');
         console.log("mongoDB configration error\n",error);
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -18,10 +18,98 @@ const User= mongoose.model("users",{
         type:Boolean,
         default:false 
     },
+    preLanguage:{
+        type:Strig,
+        default:"English",
+        required: true,
+        enum :["Sinhala", "Tamil", "English"] 
+    },
     otp: {
         type : String,
         default:null 
-    }
+    },
+    payments:[
+        {
+            amount:{
+                type: Number,
+                required: true,
+                default: 400
+            },
+            subcriDate:{
+                type: Date,
+                required: true,
+                default: Date.now
+            }
+        }
+    ],
+    refundRequests:[
+        {
+            reqDate:{
+                type: Date,
+                trim : true,
+                required : true
+            },
+            resDate:{
+                type: Date,
+                trim : true
+            },
+            Status: {
+                type: String,
+                trim : true,
+                default: "Open",
+                enum :["Opened", "Accepted", "Rejected"]
+            }
+        }
+    ],
+    isAdmin:{
+        type: Boolean,
+        required: true
+    }, 
+    query:[
+        {
+           req:{
+            type: String,
+            required: true,
+            trim: true
+           },
+           res:{
+            type: String,
+            trim: true
+           },
+           reqDate:{
+            type: Date,
+            required: true
+           },
+           resDate:{
+            type: Date,
+            required: true
+           },
+           status:{
+            type: String,
+            default: "Open",
+            required: true,
+            enum:["Open","Closed"]
+           }
+        }
+    ],
+    completed:[
+        {
+            topicId:{
+                type: Number,
+                required: true
+            },
+            isVideoFinished:{
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            isQuizFinished:{
+                type: Boolean,
+                required: true,
+                default: false
+            }
+        }
+    ]
 })
 
 module.exports = User
