adae09398df92945685bb19a820cb0a80c6f4c14
kamadi2000 <kmaliyanage@gmail.com>
2023-10-18 16:10:18 +0530

    questions display in practice quiz page

--- webapp/src/views/Exam.js
+++ webapp/src/views/Exam.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FrameComponent from '../components/FrameComponent'
 import QuestionComponent from '../components/QuestionComponent'
+import axios from 'axios'
 
 function Exam() {
+    const [question, setQuestion] = useState([])
+    useEffect(()=>{
+        axios.defaults.withCredentials = true
+        axios.get('http://localhost:5000/api/material/getQuestions')
+            .then(response => {
+                // Handle the successful response here
+                setQuestion(response.data)
+                console.log(response.data)
+                
+    
+                
+            })
+            .catch(error => {
+                console.log(error)
+    
+    
+            })
+      },[])
   return (
     <FrameComponent>
         <div className='container min-vh-100 rounded-1 mb-1 mb-md-2 mb-lg-3 mt-1 mt-md-2 mt-lg-3 col-11 col-md-10 col-lg-10 mx-auto' >
@@ -25,16 +44,19 @@ function Exam() {
                         </div>
                         <div>
                             <hr/>
-                            <QuestionComponent id={1} />
-                            <QuestionComponent id={2} />
+                            {question.map((mcq, index) => (
+                                <QuestionComponent id={index} mcq = {mcq} />
+                            ))}
+                            
+                            {/* <QuestionComponent id={2} />
                             <QuestionComponent id={3} />
                             <QuestionComponent id={4} />
                             <QuestionComponent id={5} />
-                            <QuestionComponent id={6} />
-                            <QuestionComponent id={7} />
+                            <QuestionComponent id={6} /> */}
+                            {/* <QuestionComponent id={7} />
                             <QuestionComponent id={8} />
                             <QuestionComponent id={9} />
-                            <QuestionComponent id={10} />
+                            <QuestionComponent id={10} /> */}
                         </div>
                     </div>
                 </div>
