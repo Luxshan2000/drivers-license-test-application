c1ef759072a60aa8c9b8e226fd6c5e80e34f9833
OshnMdw <gchoshanmadawa@gmail.com>
2023-10-22 15:13:55 +0530

    Nav Bar small change

--- webapp/src/components/OffCanvasNavComponent.js
+++ webapp/src/components/OffCanvasNavComponent.js
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import Profile from '../assets/images/profile-sample.png'
-import { useDeviceWidth } from '../context/DeviceWidthContext'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Profile from "../assets/images/profile-sample.png";
+import { useDeviceWidth } from "../context/DeviceWidthContext";
+import { Link } from "react-router-dom";
 
-import icon from '../assets/images/icon.png'
+import icon from "../assets/images/icon.png";
 
-function OffCanvasNavComponent({collapsed}) {
-  const isSmall = useDeviceWidth()
-  
-
-
-
+function OffCanvasNavComponent({ collapsed }) {
+  const isSmall = useDeviceWidth();
 
   return (
-    <div style={{width:isSmall ? `${collapsed? '100%': '0'}` : `${collapsed? 200: 0}px`, overflowX:'hidden', transition: `width ${isSmall? '100ms' :'200ms'} linear`}}  className="d-flex  flex-column flex-shrink-0 vh-100 m-0">
-      <div className=''>
-        <Link to="/dashboard" className=" pt-2 text-center d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+    <div
+      style={{
+        width: isSmall
+          ? `${collapsed ? "100%" : "0"}`
+          : `${collapsed ? 200 : 0}px`,
+        overflowX: "hidden",
+        transition: `width ${isSmall ? "100ms" : "200ms"} linear`,
+      }}
+      className="d-flex  flex-column flex-shrink-0 vh-100 m-0"
+    >
+      <div className="">
+        <Link
+          to="/"
+          className=" pt-2 text-center d-flex align-items-center  my-4 me-md-auto text-dark text-decoration-none"
+        >
           {/* <i style={{fontSize:"10em"}} class="bi w-100  bi-person-circle"/> */}
-          <img src={icon}  width="200px"/>
+          <img src={icon} width="200px" />
         </Link>
       </div>
-      
-      <hr/>
+
+      <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <span className=' text-info' >MAIN MENU</span>
+        <span className=" text-info">MAIN MENU</span>
         <li className="nav-item">
-          <Link to="/dashboard" className="nav-link active" >
+          <Link to="/dashboard" className="nav-link active">
             <i class="bi bi-speedometer2 pe-2"></i>
             Dashboard
           </Link>
@@ -49,7 +57,6 @@ function OffCanvasNavComponent({collapsed}) {
           </Link>
         </li>
         <li>
-          
           <Link to="/dashboard/aboutus" className="nav-link text-dark">
             <i class="bi bi-file-person pe-2"></i>
             About us
@@ -57,9 +64,9 @@ function OffCanvasNavComponent({collapsed}) {
         </li>
       </ul>
 
-      <hr/>
+      <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <span className=' text-info' >PREFERENCE</span>
+        <span className=" text-info">PREFERENCE</span>
         <li className="nav-item">
           <a href="#" className="nav-link text-dark" aria-current="page">
             <i class="bi bi-gear pe-2"></i>
@@ -67,9 +74,9 @@ function OffCanvasNavComponent({collapsed}) {
           </a>
         </li>
       </ul>
-      <hr/>
+      <hr />
     </div>
-  )
+  );
 }
 
-export default OffCanvasNavComponent
+export default OffCanvasNavComponent;
