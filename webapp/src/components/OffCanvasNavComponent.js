ab1ce032fc39bc583fb6df7158d18ee88954dc39
OshnMdw <gchoshanmadawa@gmail.com>
2023-10-24 10:35:39 +0530

    Active bar problem solved

--- webapp/src/components/OffCanvasNavComponent.js
+++ webapp/src/components/OffCanvasNavComponent.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Profile from "../assets/images/profile-sample.png";
 import { useDeviceWidth } from "../context/DeviceWidthContext";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; // Import useLocation
 
 import icon from "../assets/images/icon.png";
+// import "./YourCustomStyles.css"; // Import your custom CSS file
 
 function OffCanvasNavComponent({ collapsed }) {
   const isSmall = useDeviceWidth();
+  const location = useLocation(); // Get the current location
+
+  const [activeTab, setActiveTab] = useState(""); // State to manage the active tab
+
+  useEffect(() => {
+    // When the location changes, update the active tab based on the current pathname
+    setActiveTab(location.pathname);
+  }, [location.pathname]);
 
   return (
     <div
@@ -22,56 +30,83 @@ function OffCanvasNavComponent({ collapsed }) {
       <div className="">
         <Link
           to="/"
-          className=" pt-2 text-center d-flex align-items-center  my-4 me-md-auto text-dark text-decoration-none"
+          className="pt-2 text-center d-flex align-items-center  my-4 me-md-auto text-dark text-decoration-none"
         >
-          {/* <i style={{fontSize:"10em"}} class="bi w-100  bi-person-circle"/> */}
           <img src={icon} width="200px" />
         </Link>
       </div>
 
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <span className=" text-info">MAIN MENU</span>
+        <span className="text-info">MAIN MENU</span>
         <li className="nav-item">
-          <Link to="/dashboard" className="nav-link active">
-            <i class="bi bi-speedometer2 pe-2"></i>
-            Dashboard
+          <Link
+            to="/dashboard"
+            className={`nav-link ${activeTab === "/dashboard" ? "active" : ""}`}
+          >
+            <i class="bi bi-speedometer2 pe-2 text-dark"></i>
+            <span className="text-dark">Dashboard</span>
           </Link>
         </li>
         <li>
-          <Link to="/dashboard/course" className="nav-link text-dark">
-            <i class="bi bi-mortarboard pe-2"></i>
-            Course
+          <Link
+            to="/dashboard/course"
+            className={`nav-link ${
+              activeTab === "/dashboard/course" ? "active" : ""
+            }`}
+          >
+            <i class="bi bi-mortarboard pe-2 text-dark"></i>
+            <span className="text-dark">Course</span>
           </Link>
         </li>
         <li>
-          <Link to="/dashboard/premium" className="nav-link text-dark">
-            <i class="bi bi-credit-card pe-2"></i>
-            Premium
+          <Link
+            to="/dashboard/premium"
+            className={`nav-link ${
+              activeTab === "/dashboard/premium" ? "active" : ""
+            }`}
+          >
+            <i class="bi bi-credit-card pe-2 text-dark"></i>
+            <span className="text-dark">Premium</span>
           </Link>
         </li>
         <li>
-          <Link to="/dashboard/contactus" className="nav-link text-dark">
-            <i class="bi bi-chat-left pe-2"></i>
-            Contact us
+          <Link
+            to="/dashboard/contactus"
+            className={`nav-link ${
+              activeTab === "/dashboard/contactus" ? "active" : ""
+            }`}
+          >
+            <i class="bi bi-chat-left pe-2 text-dark"></i>
+            <span className="text-dark">Contact us</span>
           </Link>
         </li>
         <li>
-          <Link to="/dashboard/aboutus" className="nav-link text-dark">
-            <i class="bi bi-file-person pe-2"></i>
-            About us
+          <Link
+            to="/dashboard/aboutus"
+            className={`nav-link ${
+              activeTab === "/dashboard/aboutus" ? "active" : ""
+            }`}
+          >
+            <i class="bi bi-file-person pe-2 text-dark"></i>
+            <span className="text-dark">About us</span>
           </Link>
         </li>
       </ul>
 
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <span className=" text-info">PREFERENCE</span>
+        <span className="text-info">PREFERENCE</span>
         <li className="nav-item">
-          <a href="#" className="nav-link text-dark" aria-current="page">
-            <i class="bi bi-gear pe-2"></i>
-            Settings
-          </a>
+          <Link
+            to="/dashboard/updateProfile"
+            className={`nav-link ${
+              activeTab === "/dashboard/updateProfile" ? "active" : ""
+            }`}
+          >
+            <i class="bi bi-gear pe-2 text-dark"></i>
+            <span className="text-dark">Settings</span>
+          </Link>
         </li>
       </ul>
       <hr />
