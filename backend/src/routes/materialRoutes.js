2d785360757f3df271fa0002f47659b8dbefcd37
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-10-26 18:35:05 +0530

    review

--- backend/src/routes/materialRoutes.js
+++ backend/src/routes/materialRoutes.js
@@ -3,32 +3,33 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const topicController = require('../controllers/topicController');
 const questionController = require('../controllers/questionController');
+const { verifyUser } = require('../middleware');
 require('dotenv').config();
 
 
-const  verifyUser = (req,res,next) => {
-    const token = req.cookies.token
-    if(!token){
-      res.json({msg:"provide token!"})
-    }
-    else{
-      jwt.verify(token,process.env.SECURITY_KEY, (err,decoded)=>{
-        if(err){
-            try {
-                res.clearCookie('token')
-                res.json({ msg: "Done" })
-              }
-              catch (err) {
-                res.status(404).json({error:err})
-              }
-        }
-        else{
-          req.email = decoded.email
-          next()
-        }
-      })
-    }
-  }
+// const  verifyUser = (req,res,next) => {
+//     const token = req.cookies.token
+//     if(!token){
+//       res.json({msg:"provide token!"})
+//     }
+//     else{
+//       jwt.verify(token,process.env.SECURITY_KEY, (err,decoded)=>{
+//         if(err){
+//             try {
+//                 res.clearCookie('token')
+//                 res.json({ msg: "Done" })
+//               }
+//               catch (err) {
+//                 res.status(404).json({error:err})
+//               }
+//         }
+//         else{
+//           req.email = decoded.email
+//           next()
+//         }
+//       })
+//     }
+//   }
 
 router.get('/topics',verifyUser,topicController.getAllTopics)
 router.get('/topic/:id',verifyUser,topicController.getTopicScript)
