394356012990f0f93c9cdd1099edbf8e89a0e175
kamadi2000 <kmaliyanage@gmail.com>
2023-10-26 10:41:28 +0530

    middleware for mobile added

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,51 +2,29 @@ const express = require('express');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const authController = require('../controllers/authController');
+const { verifyUser } = require('../middleware')
 
 
 
-const  verifyUser = (req,res,next) => {
-    const token = req.cookies.token
-    if(!token){
-      res.json({msg:"provide token!"})
-    }
-    else{
-      jwt.verify(token,process.env.SECURITY_KEY, (err,decoded)=>{
-        if(err){
-            try {
-                res.clearCookie('token')
-                res.json({ msg: "Done" })
-              }
-              catch (err) {
-                res.status(404).json({error:err})
-              }
-        }
-        else{
-          req.email = decoded.email
-          next()
-        }
-      })
-    }
-  }
 
 
 
 router.post('/signup', authController.signUpWeb);
-router.post('/signUpApp',authController.signUpApp);
+router.post('/signUpApp', authController.signUpApp);
 
 router.post('/login', authController.loginWeb);
 router.post('/loginApp', authController.loginApp);
 
-router.post('/googleLogin',authController.googleLogin)
-router.post('/googleLoginApp',authController.googleLoginApp);
+router.post('/googleLogin', authController.googleLogin)
+router.post('/googleLoginApp', authController.googleLoginApp);
 
-router.post('/facebooklogin',authController.facebooklogin);
+router.post('/facebooklogin', authController.facebooklogin);
 
 router.post('/logout', authController.logout);
 
 router.post('/verifyotp', authController.verificationWeb);
-router.post('/verifyOtpApp',authController.verificationApp)
+router.post('/verifyOtpApp', authController.verificationApp)
 
-router.post('/resend', verifyUser ,authController.resendOTP )
+router.post('/resend', verifyUser, authController.resendOTP)
 
 module.exports = router;
