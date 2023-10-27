47ebef01622bafcd9053f8238e274788706e53f6
Luxshan2000 <Luxshanlux2000@gmail.com>
2023-10-27 18:44:16 +0530

    origin updated

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001","*"],
     methods: ["POST", "GET"],
     credentials: true,
   })
